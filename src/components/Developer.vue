4bf804a09f358654e8fb30bd22d314364693d1c9
vickzil <nwakwyevictor@gmail.com>
2020-10-19 11:40:13 +0100

    music playlist

---
+++ src/components/Developer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="developer" class="container-fluid">
+    <p>
+      <span class="about-img">
+        <img src="../assets/img/me3.jpg" />
+      </span>
+      <span class="aboutdeveloper" id="text-logo">
+        My name is VICTOR NWAKWUE, i am a passionate technology enthusiast and a very good Fullstack developer ( Frontend and Backend ).
+        <br />
+        <br />Victor started his career as a social media marketer and then moved to website development using HTML, CSS, Bootstrap, jQuery , Vue, Node, PHP andMySQL.
+        <br />
+        <br />He is a self-taught Web developer with interest in building user-friendly websites to help companies promote their products.
+        <br />
+        <br />Building websites is my passion because it is the fastest means of product marketing and also provides solutions as regards information and communication.
+        <br />
+        <br />He thrives on challenges and competitions, hard-work and always ready to get my hands dirty in code.
+        Lover of Music, football and video games.
+        <br />
+      </span>
+    </p>
+    <div class="contact_me">
+      <h4>Connect to me</h4>
+      <div class="buttons">
+        <a href="mailto:nwakwyevictor@gmail.com" class="contact_me_btn email">
+          <i class="fa fa-envelope-o mr-2"></i>
+          E-mail me
+        </a>
+        <a href="https://github.com/vickzil" class="contact_me_btn github">
+          <i class="fa fa-github mr-2"></i>
+          Github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/victor-nwakwue-910791153/"
+          class="contact_me_btn linkedin"
+        >
+          <i class="fa fa-linkedin mr-2"></i>
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Developer"
+};
+</script>
+
+<style>
+</style>
